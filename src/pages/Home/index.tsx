9d50cc49028df196b65881eaecea9d8c5a5821e3
Wagner Reis <wagnerreis1997@gmail.com>
2025-04-19 11:56:07 -0300

    feat(home): add search functionality for coffee catalog

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,17 +3,43 @@ import { Intro } from "./components/Intro";
 import { CardListContainer, NavBar } from "./style";
 
 import { coffees } from "../../utils/coffees";
+import { ChangeEvent, useCallback, useState } from "react";
+
+import { debounce } from "lodash";
 
 export function Home() {
+  const [search, setSearch] = useState("");
+
+  const handleSearchChange = useCallback((value: string) => {
+    const debouncedSearch = debounce((searchValue: string) => {
+      setSearch(searchValue);
+    }, 300);
+    debouncedSearch(value);
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleSearchChange(event.target.value);
+  };
+
+  const filteredCoffees = search.length
+    ? coffees.filter((coffee) =>
+        coffee.title.toLowerCase().includes(search.toLowerCase()),
+      )
+    : coffees;
+
   return (
     <>
       <Intro />
       <NavBar>
         <h2>Nossos cafés</h2>
-        <input type="text" placeholder="Pesquisar café..." />
+        <input
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Pesquisar café..."
+        />
       </NavBar>
       <CardListContainer>
-        {coffees.map((coffee) => {
+        {filteredCoffees.map((coffee) => {
           return <CatalogCard key={coffee.id} coffee={coffee} />;
         })}
       </CardListContainer>
